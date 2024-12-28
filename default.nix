5ff01e563e8f478ea4bcaf701e40be307252b235
sadan <117494111+sadan4@users.noreply.github.com>
2024-12-28 02:39:41 -0500

    guhhh

--- default.nix
+++ default.nix
@@ -6,60 +6,73 @@
   gnumake,
   zydis,
   callPackage,
+  fetchFromGitHub,
 }:
-stdenv.mkDerivation (finalAttrs: rec {
-  pname = "funchook";
-  name = pname;
-  src = ./.;
-  buildInputs = [
-    # (capstone.overrideAttrs (
-    #   _: old: {
-    #     src = fetchFromGitHub {
-    #       owner = "capstone-engine";
-    #       repo = "capstone";
-    #       rev = "097c04d9413c59a58b00d4d1c8d5dc0ac158ffaa";
-    #       hash = "sha256-kKmL5sae9ruWGu1gas1mel9qM52qQOD+zLj8cRE3isg";
-    #     };
-    #   }
-    # ))
-    zydis
-    # wont build outside of this repo without this
-    # reproducible builds my ass
-    (callPackage (import ./zycore.nix) {})
-  ];
-  nativeBuildInputs = [
-    cmake
-    gnumake
-    zydis
-  ];
-  configurePhase = ''
-    runHook preConfigure
+stdenv.mkDerivation (
+  finalAttrs:
+  let
+    zydisSrc = fetchFromGitHub {
+      owner = "zyantific";
+      repo = "zydis";
+      rev = "v4.1.0";
+      hash = "sha256-akusu0T7q5RX4KGtjRqqOFpW5i9Bd1L4RVZt8Rg3PJY=";
+    };
+  in
+  rec {
+    pname = "funchook";
+    name = pname;
+    src = ./.;
+    buildInputs = [
+      # (capstone.overrideAttrs (
+      #   _: old: {
+      #     src = fetchFromGitHub {
+      #       owner = "capstone-engine";
+      #       repo = "capstone";
+      #       rev = "097c04d9413c59a58b00d4d1c8d5dc0ac158ffaa";
+      #       hash = "sha256-kKmL5sae9ruWGu1gas1mel9qM52qQOD+zLj8cRE3isg";
+      #     };
+      #   }
+      # ))
+      zydis
+      # wont build outside of this repo without this
+      # reproducible builds my ass
+      (callPackage (import ./zycore.nix) { })
+    ];
+    nativeBuildInputs = [
+      cmake
+      gnumake
+      zydis
+    ];
+    configurePhase = ''
+      runHook preConfigure
 
-    mkdir build
-    pushd build
-    cmake -DCMAKE_BUILD_TYPE=${buildType} -DFUNCHOOK_DISASM=zydis ..
-    popd
+      mkdir build
+      pushd build
+      export ZYDIS_SRC=${zydisSrc}
+      cmake -DCMAKE_BUILD_TYPE=${buildType} -DFUNCHOOK_DISASM=zydis ..
+      popd
 
-    runHook postConfigure
-  '';
+      runHook postConfigure
+    '';
 
-  buildPhase = ''
-    runHook preBuild
+    buildPhase = ''
+      runHook preBuild
 
-    pushd build
-    make -j
-    popd
+      pushd build
+      make -j
+      popd
 
-    runHook postBuild
-  '';
+      runHook postBuild
+    '';
 
-  installPhase = ''
-    runHook preInstall
+    installPhase = ''
+      runHook preInstall
 
-    mkdir -p $out/include $out/lib
-    cp -r include/* $out/include
-    cp build/libfunc* $out/lib
+      mkdir -p $out/include $out/lib
+      cp -r include/* $out/include
+      cp build/libfunc* $out/lib
 
-    runHook postInstall
-  '';
-})
+      runHook postInstall
+    '';
+  }
+)
